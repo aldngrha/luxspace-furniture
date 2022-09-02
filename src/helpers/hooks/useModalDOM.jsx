ef1a7a5288bde12a4db4897bc6273febe2d0192e
aldngrha <aldi.nugrahatk@gmail.com>
2022-09-02 15:23:57 +0700

    adding custom hooks useModalDOM

--- src/helpers/hooks/useModalDOM.jsx
+++ src/helpers/hooks/useModalDOM.jsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from "react";
-import { addClass } from "../format/classNameModifier";
+import { addClass, removeClass } from "../format/classNameModifier";
 
 export default function useModalDOM() {
   useLayoutEffect(() => {
