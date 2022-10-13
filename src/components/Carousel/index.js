8bfcb1b8f647bf14899c5eeb111cefcbd495d7bf
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-13 12:47:28 +0700

    update caraousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,6 +3,7 @@ import { addClass, removeClass } from "../../helpers/format/classNameModifier";
 
 export default function Carousel({ children, refContainer }) {
   const refDragHandler = useRef(null);
+  const containerClientRect = refContainer.current.getBoundingClientRect();
   const [index, setIndex] = useState(0);
 
   const threshold = 100;
@@ -10,7 +11,7 @@ export default function Carousel({ children, refContainer }) {
   const DIRECTION_LEFT = "DIRECTION_LEFT";
   const DIRECTION_RIGHT = "DIRECTION_RIGHT";
 
-  const postInitial = useRef();
+  const posInitial = useRef();
   const posX1 = useRef();
   const posX2 = useRef();
   const posFinal = useRef();
@@ -31,15 +32,16 @@ export default function Carousel({ children, refContainer }) {
           refDragHandler.current.style.left = 0;
           setIndex(0);
         } else if (index >= cardCount - itemToShow) {
-          refDragHandler.current.style.left = -(
+          refDragHandler.current.style.left = `${-(
             (cardCount - itemToShow + isMobile) *
             cardSize
-          );
+          )}px`;
           setIndex(cardCount - itemToShow);
         } else if (index === cardCount || index === cardCount - 1) {
-          refDragHandler.current.style.left = (cardCount - 1) * cardSize;
+          refDragHandler.current.style.left = `${(cardCount - 1) * cardSize}px`;
           setIndex(cardCount - 1);
         }
+
         isAllowShift.current = true;
       }
     },
@@ -54,15 +56,16 @@ export default function Carousel({ children, refContainer }) {
         if (direction === "DIRECTION_LEFT") {
           setIndex((prev) => prev + 1);
           refDragHandler.current.style.left = `${
-            postInitial.current - cardSize
+            posInitial.current - cardSize
           }px`;
         } else if (direction === "DIRECTION_RIGHT") {
           setIndex((prev) => prev - 1);
           refDragHandler.current.style.left = `${
-            postInitial.current + cardSize
+            posInitial.current + cardSize
           }px`;
         }
       }
+
       isAllowShift.current = false;
     },
     [cardSize]
@@ -88,29 +91,33 @@ export default function Carousel({ children, refContainer }) {
     [posX1, posX2]
   );
 
-  const onDragEnd = useCallback((e) => {
-    e = e || window.event;
-    e.preventDefault();
+  const onDragEnd = useCallback(
+    (e) => {
+      e = e || window.event;
+      e.preventDefault();
 
-    posFinal.current = refDragHandler.offsetLeft;
+      posFinal.current = refDragHandler.current.offsetLeft;
 
-    if (posFinal.current - posFinal.current < -threshold) {
-      fnShiftItem(DIRECTION_LEFT);
-    } else if (posFinal.current - posFinal.current > threshold) {
-      fnShiftItem(DIRECTION_RIGHT);
-    } else {
-      refDragHandler.current.style.left = `${postInitial.current}px`;
-    }
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }, []);
+      if (posFinal.current - posInitial.current < -threshold) {
+        fnShiftItem(DIRECTION_LEFT);
+      } else if (posFinal.current - posInitial.current > threshold) {
+        fnShiftItem(DIRECTION_RIGHT);
+      } else {
+        refDragHandler.current.style.left = `${posInitial.current}px`;
+      }
+
+      document.onmouseup = null;
+      document.onmousemove = null;
+    },
+    [fnShiftItem]
+  );
 
   const onDragStart = useCallback(
     (e) => {
       e = e || window.event;
       e.preventDefault();
 
-      postInitial.current = refDragHandler.current.offsetLeft;
+      posInitial.current = refDragHandler.current.offsetLeft;
 
       if (e.type === "touchstart") {
         posX1.current = e.touches[0].clientX;
@@ -123,31 +130,41 @@ export default function Carousel({ children, refContainer }) {
     [onDragEnd, onDragMove]
   );
 
-  const onClick = (e) => {
+  const onClick = useCallback((e) => {
     e = e || window.event;
     !isAllowShift.current && e.preventDefault();
-  };
+  }, []);
 
   useLayoutEffect(() => {
     const refForwardDragHandler = refDragHandler.current;
 
+    refForwardDragHandler.onmousedown = onDragStart;
     refForwardDragHandler.addEventListener("touchstart", onDragStart);
-    refForwardDragHandler.addEventListener("touchmove", onDragEnd);
-    refForwardDragHandler.addEventListener("touchstart", onDragMove);
+    refForwardDragHandler.addEventListener("touchend", onDragEnd);
+    refForwardDragHandler.addEventListener("touchmove", onDragMove);
     refForwardDragHandler.addEventListener("click", onClick);
     refForwardDragHandler.addEventListener("transitionend", fnCheckIndex);
-
     return () => {
       refForwardDragHandler.removeEventListener("touchstart", onDragStart);
-      refForwardDragHandler.removeEventListener("touchmove", onDragEnd);
-      refForwardDragHandler.removeEventListener("touchstart", onDragMove);
+      refForwardDragHandler.removeEventListener("touchend", onDragEnd);
+      refForwardDragHandler.removeEventListener("touchmove", onDragMove);
       refForwardDragHandler.removeEventListener("click", onClick);
       refForwardDragHandler.removeEventListener("transitionend", fnCheckIndex);
     };
-  }, [onDragStart, onDragMove, onDragEnd, onClick, fnCheckIndex]);
+  }, [onDragStart, onDragEnd, onDragMove, onClick, fnCheckIndex]);
+
+  useLayoutEffect(() => {
+    if (refDragHandler.current) {
+      cards.current = refDragHandler.current.getElementsByClassName("card");
+    }
+  }, []);
 
   return (
-    <div ref={refDragHandler} className="flex -mx-4 flex-row relative">
+    <div
+      ref={refDragHandler}
+      className="flex -mx-4 flex-row relative"
+      style={{ paddingLeft: containerClientRect.left - 16 }}
+    >
       {children}
     </div>
   );
