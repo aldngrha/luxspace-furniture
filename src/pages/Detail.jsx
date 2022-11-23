bc76fa610df96df84c17b7261d58321f77a65ee5
aldngrha <aldi.nugrahatk@gmail.com>
2022-11-23 13:03:19 +0700

    get data form API

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import Header from "../parts/Header";
 import Breadcrumb from "../components/Breadcrumb/index";
+
 import ProductDetail from "../parts/Details/ProductDetail";
 import Suggestion from "../parts/Details/Suggestion";
 import Sitemap from "../parts/Sitemap";
 import Footer from "../parts/Footer";
 
+import useAsync from "../helpers/hooks/useAsync";
+import fetch from "../helpers/fetch";
+
 export default function Detail() {
+  const { idp } = useParams();
+  const { data, run, isLoading } = useAsync();
+
+  React.useEffect(() => {
+    run(fetch({ url: `/api/products/${idp}` }));
+  }, [run]);
+
   return (
     <>
       <Header theme="black" />
@@ -17,8 +30,13 @@ export default function Detail() {
           { url: "/categories/1231/products/1", name: "Detail" },
         ]}
       />
-      <ProductDetail />
-      <Suggestion />
+      {isLoading ? "Loading" : <ProductDetail data={data} />}
+
+      {isLoading ? (
+        "Loading"
+      ) : (
+        <Suggestion data={data?.relatedProducts || {}} />
+      )}
       <Sitemap />
       <Footer />
     </>
