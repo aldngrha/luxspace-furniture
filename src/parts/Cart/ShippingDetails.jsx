33e75e879dd61ecfa62787ba2e2cb942affe341e
aldngrha <aldi.nugrahatk@gmail.com>
2022-12-17 12:51:07 +0700

    fetching meta data couriers and payments

--- src/parts/Cart/ShippingDetails.jsx
+++ src/parts/Cart/ShippingDetails.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import useAsync from "../../helpers/hooks/useAsync";
+import useForm from "../../helpers/hooks/useForm";
+import fetch from "../../helpers/fetch";
 
 export default function ShippingDetails() {
+  const { data, run, isLoading } = useAsync();
+  const { state: payload, fnUpdateState } = useForm({
+    completeName: "",
+    emailAddress: "",
+    address: "",
+    phoneNumber: "",
+    courier: "",
+    payment: "",
+  });
+
+  useEffect(() => {
+    run(fetch({ url: "/api/checkout/meta" }));
+  }, [run]);
+
   return (
     <div className="w-full md:px-4 md:w-4/12" id="shipping-detail">
       <div className="bg-gray-100 px-4 py-6 md:p-8 md:rounded-3xl">
@@ -10,26 +28,28 @@ export default function ShippingDetails() {
           </div>
 
           <div className="flex flex-col mb-4">
-            <label htmlFor="complete-name" className="text-sm mb-2">
+            <label htmlFor="completeName" className="text-sm mb-2">
               Complete Name
             </label>
             <input
-              data-input
+              onChange={fnUpdateState}
+              value={payload.completeName}
               type="text"
-              id="complete-name"
+              name="completeName"
               className="border-gray-200 border rounded-lg px-4 py-2 bg-white text-sm focus:border-blue-200 focus:outline-none"
               placeholder="Input your name"
             />
           </div>
 
           <div className="flex flex-col mb-4">
-            <label htmlFor="email" className="text-sm mb-2">
+            <label htmlFor="emailAddress" className="text-sm mb-2">
               Email Address
             </label>
             <input
-              data-input
+              onChange={fnUpdateState}
+              value={payload.emailAddress}
               type="email"
-              id="email"
+              name="emailAddress"
               className="border-gray-200 border rounded-lg px-4 py-2 bg-white text-sm focus:border-blue-200 focus:outline-none"
               placeholder="Input your email address"
             />
@@ -40,22 +60,24 @@ export default function ShippingDetails() {
               Address
             </label>
             <input
-              data-input
+              onChange={fnUpdateState}
+              value={payload.address}
               type="text"
-              id="address"
+              name="address"
               className="border-gray-200 border rounded-lg px-4 py-2 bg-white text-sm focus:border-blue-200 focus:outline-none"
               placeholder="Input your address"
             />
           </div>
 
           <div className="flex flex-col mb-4">
-            <label htmlFor="phone-number" className="text-sm mb-2">
+            <label htmlFor="phoneNumber" className="text-sm mb-2">
               Phone Number
             </label>
             <input
-              data-input
+              onChange={fnUpdateState}
+              value={payload.phoneNumber}
               type="tel"
-              id="phone-number"
+              name="phoneNumber"
               className="border-gray-200 border rounded-lg px-4 py-2 bg-white text-sm focus:border-blue-200 focus:outline-none"
               placeholder="Input your phone number"
             />
@@ -66,34 +88,36 @@ export default function ShippingDetails() {
               Choose Courier
             </label>
             <div className="flex -mx-2 flex-wrap">
-              <div className="px-2 w-6/12 h-24 mb-4">
-                <button
-                  type="button"
-                  data-value="fedex"
-                  data-name="courier"
-                  className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
-                >
-                  <img
-                    src="/images/content/logo-fedex.svg"
-                    alt="Logo Fedex"
-                    className="object-contain max-h-full"
-                  />
-                </button>
-              </div>
-              <div className="px-2 w-6/12 h-24 mb-4">
-                <button
-                  type="button"
-                  data-value="dhl"
-                  data-name="courier"
-                  className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
-                >
-                  <img
-                    src="/images/content/logo-dhl.svg"
-                    alt="Logo dhl"
-                    className="object-contain max-h-full"
-                  />
-                </button>
-              </div>
+              {isLoading
+                ? Array(2)
+                    .fill()
+                    .map((_, index) => (
+                      <div key={index} className="px-2 h-24 mb-4 w-6/12">
+                        <div className="bg-gray-300 w-full h-full animate-pulse rounded-lg mx-2"></div>
+                      </div>
+                    ))
+                : data?.couriers?.map((item) => (
+                    <div className="px-2 w-6/12 h-24 mb-4">
+                      <button
+                        type="button"
+                        onClick={() =>
+                          fnUpdateState({
+                            target: {
+                              name: "courier",
+                              value: item.id,
+                            },
+                          })
+                        }
+                        className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
+                      >
+                        <img
+                          src={item.imgUrl}
+                          alt={item.name}
+                          className="object-contain max-h-full"
+                        />
+                      </button>
+                    </div>
+                  ))}
             </div>
           </div>
 
@@ -102,60 +126,36 @@ export default function ShippingDetails() {
               Choose Payment
             </label>
             <div className="flex -mx-2 flex-wrap">
-              <div className="px-2 w-6/12 h-24 mb-4">
-                <button
-                  type="button"
-                  data-value="midtrans"
-                  data-name="payment"
-                  className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
-                >
-                  <img
-                    src="/images/content/logo-midtrans.png"
-                    alt="Logo midtrans"
-                    className="object-contain max-h-full"
-                  />
-                </button>
-              </div>
-              <div className="px-2 w-6/12 h-24 mb-4">
-                <button
-                  type="button"
-                  data-value="mastercard"
-                  data-name="payment"
-                  className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
-                >
-                  <img
-                    src="/images/content/logo-mastercard.svg"
-                    alt="Logo mastercard"
-                  />
-                </button>
-              </div>
-              <div className="px-2 w-6/12 h-24 mb-4">
-                <button
-                  type="button"
-                  data-value="bitcoin"
-                  data-name="payment"
-                  className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
-                >
-                  <img
-                    src="/images/content/logo-bitcoin.svg"
-                    alt="Logo bitcoin"
-                    className="object-contain max-h-full"
-                  />
-                </button>
-              </div>
-              <div className="px-2 w-6/12 h-24 mb-4">
-                <button
-                  type="button"
-                  data-value="american-express"
-                  data-name="payment"
-                  className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
-                >
-                  <img
-                    src="/images/content/logo-american-express.svg"
-                    alt="Logo american-logo-american-express"
-                  />
-                </button>
-              </div>
+              {isLoading
+                ? Array(2)
+                    .fill()
+                    .map((_, index) => (
+                      <div key={index} className="px-2 h-24 mb-4 w-6/12">
+                        <div className="bg-gray-300 w-full h-full animate-pulse rounded-lg mx-2"></div>
+                      </div>
+                    ))
+                : data?.payments?.map((item) => (
+                    <div className="px-2 w-6/12 h-24 mb-4">
+                      <button
+                        type="button"
+                        onClick={() =>
+                          fnUpdateState({
+                            target: {
+                              name: "payment",
+                              value: item.id,
+                            },
+                          })
+                        }
+                        className="border border-gray-200 focus:border-red-200 flex items-center justify-center rounded-xl bg-white w-full h-full focus:outline-none"
+                      >
+                        <img
+                          src={item.imgUrl}
+                          alt={item.name}
+                          className="object-contain max-h-full"
+                        />
+                      </button>
+                    </div>
+                  ))}
             </div>
           </div>
           <div className="text-center">
