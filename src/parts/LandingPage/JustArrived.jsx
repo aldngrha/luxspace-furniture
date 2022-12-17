3eecbf70213e9638f6386483ab8a88fc60a36664
aldngrha <aldi.nugrahatk@gmail.com>
2022-12-17 16:03:07 +0700

    import format currency and thousand

--- src/parts/LandingPage/JustArrived.jsx
+++ src/parts/LandingPage/JustArrived.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import useAsync from "../../helpers/hooks/useAsync";
 import fetch from "../../helpers/fetch";
+import "../../helpers/format/currency";
 import Carousel from "../../components/Carousel";
 
 function Loading() {
@@ -87,7 +88,7 @@ export default function JustArrived() {
                     />
                   </div>
                   <h5 className="text-lg font-semibold mt-4">{item.title}</h5>
-                  <span className="">IDR {item.price}</span>
+                  <span className="">{item.price.currency()}</span>
                   <Link
                     to={`categories/${item.idc}/products/${item.id}`}
                     className="stretched-link"
